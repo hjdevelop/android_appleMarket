f613b75f2741a6fb9b35ad9894ed3b38cb5e6d4d
hjdevelop <hyunjun6133@naver.com>
2023-08-31 17:49:08 +0900

    [FEAT] : ⚙️ 상세 페이지 이동 및 객체 데이터 전달

--- app/src/main/java/com/example/android_applemarket/DetailActivity.kt
+++ app/src/main/java/com/example/android_applemarket/DetailActivity.kt
@@ -1,11 +1,36 @@
 package com.example.android_applemarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.example.android_applemarket.databinding.ActivityDetailBinding
+import java.text.DecimalFormat
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent: Intent = getIntent()
+        val data: Product? = intent.getParcelableExtra("productData")
+
+        binding.productImageDetailImageView.setImageDrawable(data?.let {
+            ResourcesCompat.getDrawable(resources, it.productImage, null)
+        })
+        binding.nameDetailTextView.text = data?.seller
+        binding.addressDetailTextView.text = data?.address
+        binding.productTitleDetailTextView.text = data?.productTitle
+        binding.productWriteDetailTextView.text = data?.productWrite
+        binding.priceDetailTextView.text = getDecimalFormat(data?.price)
+
+    }
+    private fun getDecimalFormat(number: Int?): String {
+        val deciamlFormat = DecimalFormat("#,###")
+        return deciamlFormat.format(number) + "원"
     }
 }
--- app/src/main/java/com/example/android_applemarket/Product.kt
+++ app/src/main/java/com/example/android_applemarket/Product.kt
@@ -1,4 +1,17 @@
 package com.example.android_applemarket
 
-data class Product (val productNum: Int, val productImage: Int, val productTitle: String, val productWrite: String, val seller: String, val price: Int, val address: String, val likeCount: Int, val chattingCount: Int){
-}
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Product(
+    val productNum: Int,
+    val productImage: Int,
+    val productTitle: String,
+    val productWrite: String,
+    val seller: String,
+    val price: Int,
+    val address: String,
+    val likeCount: Int,
+    val chattingCount: Int
+): Parcelable
